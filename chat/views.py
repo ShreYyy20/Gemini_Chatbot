ff87498d8a4e8d578d62a7a0428be0d69e01fb21
Shreyans Chheda <90379825+ShreYyy20@users.noreply.github.com>
2024-06-13 19:22:29 +0530

    updated files

--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,8 @@ from geminichatbot.settings import GENERATIVE_AI_KEY
 from chat.models import ChatMessage, Chat
 import google.generativeai as genai
 
+from django.urls import reverse
+
 def send_message(request, chat_id):
     chat = get_object_or_404(Chat, id=chat_id)
 
@@ -13,14 +15,12 @@ def send_message(request, chat_id):
         user_message = request.POST.get('user_message')
         bot_response = model.generate_content(user_message)
 
-        ChatMessage.objects.create(chat=chat, user_message=user_message, bot_response=bot_response.text)
+        if bot_response.text:
+            ChatMessage.objects.create(chat=chat, user_message=user_message, bot_response=bot_response.text)
+        else:
+            pass
 
-    return redirect('list_messages', chat_id=chat_id)
-
-def format_bot_response(response):
-    # Here you can process the response to format it properly
-    # For simplicity, we'll just wrap code blocks in <pre> tags
-    return response.replace('```', '<pre>').replace('```', '</pre>')
+    return redirect(reverse('list_messages_with_id', kwargs={'chat_id': chat_id}))
 
 def list_messages(request, chat_id=None):
     if chat_id:
@@ -28,5 +28,5 @@ def list_messages(request, chat_id=None):
     else:
         chat = Chat.objects.first() or Chat.objects.create()
 
-    messages = chat.messages.all().order_by('-id')[:4][::-1]  # Fetch only the last 3 messages
+    messages = chat.messages.all().order_by('-id')[:4][::-1]  
     return render(request, 'chat/chat.html', {'messages': messages, 'chat_id': chat.id})
